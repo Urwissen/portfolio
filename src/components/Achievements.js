1394cca889ae43d5e5ebe7a3b5f0da1e91749a30
Lars <l.gessner@posteo.de>
2022-04-16 00:55:50 +0200

    add achivement data fetcher

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,18 +1,26 @@
+import React from "react";
+export const Achievements = (props) => {
+    const [profile, setProfile] = React.useState("name")
+    async function fetchingData() {
+        const response = await fetch("https://teamtreehouse.com/larsgessner.json")
+        const data = await response.json()
+        console.log(data)
+        setProfile(data)
+    }
 
-/*
-async function fetchingData() {
-    const response = await fetch("https://swapi.dev/api/people/1/")
-    const data = await response.json
-    return data.name
-}
+    React.useEffect(() => {
+        fetchingData()    
+    }, [])
 
-*/
 
-export const Achievements = (params) => {
     return(
         <div className="achievements">
             <p>First</p>
             <p>Some Content</p>
+            <p>{profile.name}</p>
+            <p>Total points: {profile.points.total}</p>
+            <p>JavaScript: {profile.points.JavaScript}</p>
         </div>
+
     )
 }
