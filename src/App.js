3924e9e4688e9ef1eb9a31b5d10f3e65fa0328d5
Lars <l.gessner@posteo.de>
2022-04-22 13:16:53 +0200

    colormode listener

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, {useWindowSize} from "react";
+import React from "react";
 import './App.css';
 import {Header} from './components/Header.js';
 import {Main} from './components/Main.js';
 import {Footer} from './components/Footer.js';
 
 function App() {
+  const preferDarkQuery = '(prefers-color-scheme: dark)'
   const [darkMode, setDarkMode] = React.useState("")
   const [round, setRound] = React.useState(0)
+
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarkQuery)
+    setDarkMode(mediaQuery.matches ? "dark" : "")
+  }, [])
+
   
   function toggleDarkMode() {
       if (!darkMode) {
