369a77921bf4f5193c251f097482db9cdb2d3a17
Lars <l.gessner@posteo.de>
2022-04-25 10:29:57 +0200

    change popup box content

--- src/components/PopupBox.js
+++ src/components/PopupBox.js
@@ -3,13 +3,13 @@ import React from 'react'
 function PopupBox(props) {
     const { handlePopup, content } = props
 
-    const cv = <h1>I am your CV, print me!</h1>
-    const hire = <h1>I am your contact form to make it easy for hiring me!</h1>
+    const cv = <h4>I am your CV, print me!</h4>
+    const hire = <h4>I am your contact form to make it easy for hiring me!</h4>
 
     return (
         <div className="popup-box">
             <button className="popup-box-btn" onClick={(event) => handlePopup(event, "close")}>Close</button>
-            <h2>I POP UP!</h2>
+            <h3>I POP UP!</h3>
             {content === "hire" ? hire : cv}
             <p>Contact me! Talk with me! Work with me!</p>
         </div>
